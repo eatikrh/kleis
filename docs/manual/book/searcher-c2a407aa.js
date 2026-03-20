7a448be6d4097b2e53593a24fe3df91ca3d88b72
Engin Atik <98592544+eatikrh@users.noreply.github.com>
2026-03-20 06:53:26 -0500

    Add Minuet in G screenshot to Sheet Music manual chapter

--- docs/manual/book/searcher-c2a407aa.js
+++ docs/manual/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b384b874.js',
+                path_to_root + 'searchindex-4796f6dc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
